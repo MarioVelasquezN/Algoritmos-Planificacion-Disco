04a27889af2e8139b685c65759fe5f4ef487abbe
MarioVelasquezN <rolando.velasquez18@hotmail.com>
2022-03-09 11:01:58 -0600

    fifofinish

--- main.cpp
+++ main.cpp
@@ -3,9 +3,28 @@
 
 int main () 
 {
-    cout<<"MENU\n";
-    cout<<"Crear Disco";
-    cout<<"Algoritmo FIFO";
-    cout<<"Algoritmo SSTF";
-    cout<<"Algoritmo SCAN";
+    int op=0;
+    disco d;
+    char name[30]="Disco";
+
+    do{
+
+        cout<<"MENU\n";
+        cout<<"1.Crear Disco"<<endl;
+        cout<<"2.Algoritmo FIFO"<<endl;
+        cout<<"3.Algoritmo SSTF"<<endl;
+        cout<<"4.Algoritmo SCAN"<<endl;
+        cout<<"5.SALIR"<<endl;
+        cin>>op;
+
+        if(op==1){
+            d.createDisk(name);     
+        }else if(op==2){
+            d.fifo();
+        }else if(op==3){
+            
+        }
+
+    }while(op!=5);
+    
 }
